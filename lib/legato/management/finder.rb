5dd34f69da9630141ada7f365162b88969282404
bfeigin <bbfeigin@gmail.com>
2016-07-14 09:49:19 -0700

    Respect multiple pages from GA responses with a nextLink attribute

--- lib/legato/management/finder.rb
+++ lib/legato/management/finder.rb
@@ -1,6 +1,7 @@
 module Legato
   module Management
     module Finder
+
       def base_uri
         "https://www.googleapis.com/analytics/v3/management"
       end
@@ -14,10 +15,31 @@ module Legato
           base_uri + path
         end
 
-        json = user.access_token.get(base_uri + path).body
-        items = MultiJson.decode(json).fetch('items', [])
-        items.map {|item| new(item, user)}
+        collect_items(user, uri).map do |item|
+          new(item, user)
+        end
       end
+
+      private
+        def items_key; "items"; end
+        def next_link_key; "nextLink"; end
+
+        def collect_items(user, base_uri)
+          next_uri = base_uri
+          item_collection = []
+
+          while next_uri
+            url_result = url_fetch_for_user(user, next_uri)
+            item_collection.concat(url_result.fetch(items_key, []))
+            next_uri = url_result.fetch(next_link_key, nil)
+          end
+
+          item_collection
+        end
+
+        def url_fetch_for_user(user, path)
+          MultiJson.decode(user.access_token.get(path).body)
+        end
     end
   end
 end
--- spec/support/examples/management_finder.rb
+++ spec/support/examples/management_finder.rb
@@ -1,29 +1,64 @@
 shared_examples_for "a management finder" do
-  let(:response) { stub(:body => 'some json') }
-  let(:access_token) { access_token = stub(:get => response) }
-  let(:user) { stub(:access_token => access_token, :api_key => nil) }
-
-  after do
-    user.should have_received(:access_token)
-    access_token.should have_received(:get).with('https://www.googleapis.com/analytics/v3/management'+described_class.default_path)
-    response.should have_received(:body)
-    MultiJson.should have_received(:decode).with('some json')
+  def full_path(path)
+    'https://www.googleapis.com/analytics/v3/management' + path
   end
 
-  it "returns an array of all #{subject_class_name} available to a user" do
-    MultiJson.stubs(:decode).returns({'items' => ['item1', 'item2']})
-    described_class.stubs(:new).returns('thing1', 'thing2')
+  describe "with a single page of #{subject_class_name}" do
+    let(:response) { stub(:body => 'some json') }
+    let(:access_token) { access_token = stub(:get => response) }
+    let(:user) { stub(:access_token => access_token, :api_key => nil) }
 
-    described_class.all(user).should == ['thing1', 'thing2']
+    after do
+      user.should have_received(:access_token)
+      access_token.should have_received(:get).with(full_path(described_class.default_path))
+      response.should have_received(:body)
+      MultiJson.should have_received(:decode).with('some json')
+    end
 
-    described_class.should have_received(:new).with('item1', user)
-    described_class.should have_received(:new).with('item2', user)
+    it "returns an array of all #{subject_class_name} available to a user" do
+      MultiJson.stubs(:decode).returns({'items' => ['item1', 'item2']})
+      described_class.stubs(:new).returns('thing1', 'thing2')
+
+      described_class.all(user).should == ['thing1', 'thing2']
+
+      described_class.should have_received(:new).with('item1', user)
+      described_class.should have_received(:new).with('item2', user)
+    end
+
+    it "returns an empty array of #{subject_class_name} when there are no results" do
+      MultiJson.stubs(:decode).returns({})
+      described_class.all(user).should == []
+
+      described_class.should have_received(:new).never
+    end
   end
 
-  it "returns an empty array of #{subject_class_name} when there are no results" do
-    MultiJson.stubs(:decode).returns({})
-    described_class.all(user).should == []
+  describe "with a paged response for #{subject_class_name}" do
+    let(:first_url) {full_path("/first_url")}
+    let(:next_url) {full_path("/next_url")}
+    let(:first_response) do
+      stub(:body => MultiJson.encode({"items" => ["item_1"], "nextLink" => next_url} ))
+    end
+    let(:second_response) do
+      stub(:body => MultiJson.encode({"items" => ["item_2"]} ))
+    end
+    let(:access_token) do
+      access_token = stub
+      access_token.stubs(:get).returns(first_response, second_response)
+      access_token
+    end
+    let(:large_user) do
+      user = stub
+      user.stubs(:access_token).returns(access_token)
+      user.stubs(:api_key).returns(nil)
+      user
+    end
 
-    described_class.should have_received(:new).never
+    it "returns an array of all #{subject_class_name}" do
+      res = described_class.all(large_user, first_url)
+      res.each do |result|
+        expect(result.class).to eq(described_class)
+      end
+    end
   end
 end
